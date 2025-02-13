f1769155199669c3a726f15de3f6521e32d80600
execution720 <JustTomGo@gmail.com>
2025-02-13 21:00:31 +0900

    test(ButtonDirectVotingKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonDirectVotingKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonDirectVotingKtTest.kt
@@ -1,12 +1,31 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonDirectVotingKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
+    // ButtonDirectVoting click 여부를 확인
     @Test
-    fun buttonDirectVoting() {
+    fun onClickTest() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonDirectVoting {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonDirectVoting").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonDirectVoting").performClick()
+
+        assertTrue(isClicked)
     }
 }
