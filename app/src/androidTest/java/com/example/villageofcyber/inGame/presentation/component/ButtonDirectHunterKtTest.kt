cfb952647a8e43b38f27158490ecf2e97b41d7ca
execution720 <JustTomGo@gmail.com>
2025-02-13 20:52:19 +0900

    test(ButtonDirectHunterKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonDirectHunterKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonDirectHunterKtTest.kt
@@ -1,12 +1,31 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonDirectHunterKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
+    // ButtonDirectHunter click 여부를 확인
     @Test
-    fun buttonDirectHunter() {
+    fun onClickTest() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonDirectHunter {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonDirectHunter").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonDirectHunter").performClick()
+
+        assertTrue(isClicked)
     }
 }
