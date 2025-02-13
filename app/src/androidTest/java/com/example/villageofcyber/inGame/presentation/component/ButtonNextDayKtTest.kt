976a2c63c43b8c3b683327c6a0c84d6090fb1148
execution720 <JustTomGo@gmail.com>
2025-02-13 18:03:48 +0900

    test(ButtonNextDayKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonNextDayKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonNextDayKtTest.kt
@@ -1,12 +1,30 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonNextDayKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
     @Test
-    fun buttonNextDay() {
+    fun `onClickTest`() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonNextDay {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonNextDay").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonNextDay").performClick()
+
+        assertTrue(isClicked)
     }
 }
