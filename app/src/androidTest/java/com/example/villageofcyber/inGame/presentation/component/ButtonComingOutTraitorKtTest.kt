b7b9f6ac5441c89956bb0bc3bd230189a552ca25
execution720 <JustTomGo@gmail.com>
2025-02-13 20:14:34 +0900

    test(ButtonComingOutTraitorKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutTraitorKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutTraitorKtTest.kt
@@ -1,12 +1,30 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonComingOutTraitorKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
     @Test
-    fun buttonComingOutTraitor() {
+    fun onClickTest() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonComingOutTraitor {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutTraitor").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutTraitor").performClick()
+
+        assertTrue(isClicked)
     }
 }
