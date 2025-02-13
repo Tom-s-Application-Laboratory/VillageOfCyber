48f691cea8c804cb1ed183946ed00f1495afc964
execution720 <JustTomGo@gmail.com>
2025-02-13 20:05:33 +0900

    test(ButtonComingOutProphetKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutProphetKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutProphetKtTest.kt
@@ -1,12 +1,30 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonComingOutProphetKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
     @Test
-    fun buttonComingOutProphet() {
+    fun `onClickTest`() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonComingOutProphet {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutProphet").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutProphet").performClick()
+
+        assertTrue(isClicked)
     }
 }
