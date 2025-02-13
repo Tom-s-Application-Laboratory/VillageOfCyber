6480caf9c5401faa8fcd02a1a9538db7884b8f9d
execution720 <JustTomGo@gmail.com>
2025-02-13 20:44:29 +0900

    test(ButtonDirectProphetKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonDirectProphetKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonDirectProphetKtTest.kt
@@ -1,12 +1,31 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonDirectProphetKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
+    // ButtonDirectProphet click 여부를 확인
     @Test
-    fun buttonDirectProphet() {
+    fun onClickTest() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonDirectProphet {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonDirectProphet").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonDirectProphet").performClick()
+
+        assertTrue(isClicked)
     }
 }
