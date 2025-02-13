74c4efac4be94243c42b81f8b658e44bcc24e992
execution720 <JustTomGo@gmail.com>
2025-02-13 17:41:24 +0900

    test(ButtonOpenCommandMenuKtTest): onClickTest

---
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonOpenCommandMenuKtTest.kt
@@ -0,0 +1,30 @@
+package com.example.villageofcyber.inGame.presentation.component
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import org.junit.Assert.*
+import org.junit.Rule
+
+import org.junit.Test
+
+class ButtonOpenCommandMenuKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun `onClickTest`() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonOpenCommandMenu {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonOpenCommandMenu").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonOpenCommandMenu").performClick()
+
+        assertTrue(isClicked)
+    }
+}
