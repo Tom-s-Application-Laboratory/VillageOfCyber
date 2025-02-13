7ffd84d11f505ace699e40dfb13468b388d73738
execution720 <JustTomGo@gmail.com>
2025-02-13 19:50:08 +0900

    test(ButtonComingOutCoworkerKtTest): onClickTest

---
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutCoworkerKtTest.kt
@@ -0,0 +1,30 @@
+package com.example.villageofcyber.inGame.presentation.component
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import org.junit.Assert.*
+import org.junit.Rule
+
+import org.junit.Test
+
+class ButtonComingOutCoworkerKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun `onClickTest`() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonComingOutCoworker {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutCoworker").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutCoworker").performClick()
+
+        assertTrue(isClicked)
+    }
+}
