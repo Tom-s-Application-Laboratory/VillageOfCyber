7bae399e4eb116c6694d55b1591fba3fe8a8897f
execution720 <JustTomGo@gmail.com>
2025-02-13 17:39:09 +0900

    feat(ButtonOpenCommandMenu)

---
+++ app/src/main/java/com/example/villageofcyber/inGame/presentation/component/ButtonOpenCommandMenu.kt
@@ -0,0 +1,36 @@
+package com.example.villageofcyber.inGame.presentation.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.villageofcyber.R
+
+@Composable
+fun ButtonOpenCommandMenu(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Image(
+        modifier = modifier
+            .clickable {
+                onClick()
+            }
+            .testTag(tag = "ButtonOpenCommandMenu"),
+        painter = painterResource(R.drawable.button_open_command_menu),
+        contentScale = ContentScale.FillBounds,
+        contentDescription = null
+    )
+}
+
+@Preview
+@Composable
+private fun ButtonOpenCommandMenuPreview() {
+    ButtonOpenCommandMenu(
+        onClick = {}
+    )
+}
