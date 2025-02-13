ee9dcca14b61a9fbd0c31b809657473ee5160408
execution720 <JustTomGo@gmail.com>
2025-02-13 20:32:39 +0900

    test(ButtonComingOutHunterKtTest): onClickTest

--- app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutHunterKtTest.kt
+++ app/src/androidTest/java/com/example/villageofcyber/inGame/presentation/component/ButtonComingOutHunterKtTest.kt
@@ -1,12 +1,31 @@
 package com.example.villageofcyber.inGame.presentation.component
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ButtonComingOutHunterKtTest {
+    @get: Rule
+    val composeTestRule = createComposeRule()
 
+    // ButtonComingOutHunter click 여부를 확인
     @Test
-    fun buttonComingOutHunter() {
+    fun onClickTest() {
+        var isClicked = false
+
+        composeTestRule.setContent {
+            ButtonComingOutHunter {
+                isClicked = true
+            }
+        }
+
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutHunter").assertExists()
+        composeTestRule.onNodeWithTag(testTag = "ButtonComingOutHunter").performClick()
+
+        assertTrue(isClicked)
     }
 }
