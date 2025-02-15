1af91110aa380d7f165493a6fdcf5b4701afd41c
execution720 <JustTomGo@gmail.com>
2025-02-15 19:51:09 +0900

    chore(build.gradle.kts): upload build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // @Serializable 어노테이션을 사용하여 직렬화를 하기 위함.
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
 android {
@@ -49,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     testImplementation(libs.junit.junit)
     testImplementation(libs.junit.junit)
@@ -58,4 +62,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // @Serializable 어노테이션을 사용하여 직렬화를 하기 위함.
+    implementation(libs.kotlinx.serialization.json.v180)
+
+    // navigation 사용을 위함.
+    implementation(libs.androidx.navigation.compose)
 }
