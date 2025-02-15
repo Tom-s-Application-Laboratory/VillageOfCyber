e3da6a877e0468ca335fd943da0c0c53ddd3ada9
execution720 <JustTomGo@gmail.com>
2025-02-15 23:20:19 +0900

    feat(NavigationRoot): implement navigation system

--- app/src/main/java/com/example/villageofcyber/core/navigation/NavigationRoot.kt
+++ app/src/main/java/com/example/villageofcyber/core/navigation/NavigationRoot.kt
@@ -1,11 +1,13 @@
 package com.example.villageofcyber.core.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavGraphBuilder
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
+import com.example.villageofcyber.R
 import com.example.villageofcyber.inGame.presentation.screen.InGameScreen
 import com.example.villageofcyber.levelSelection.presentation.screen.LevelSelectionScreen
 import com.example.villageofcyber.title.presentation.screen.TitleScreen
@@ -13,39 +15,64 @@ import kotlinx.serialization.Serializable
 
 @Composable
 fun NavigationRoot(
-    navController: NavHostController
+    navController: NavHostController,
+    innerPadding: PaddingValues
 ) {
     NavHost(
         navController = navController,
         startDestination = Route.TitleMenu
     ) {
-        gameGraph(navController)
-    }
-
-}
-
-private fun NavGraphBuilder.gameGraph(navController: NavHostController) {
-    navigation<Route>(
-        startDestination = Route.TitleMenu
-    ) {
         composable<Route.TitleMenu> {
-            TitleScreen {       // edge 조절을 해주려면 modifier를 뚫어줘야 할 듯.
-
+            TitleScreen(
+                modifier = Modifier
+                    .padding(innerPadding)
+            ) {
+                navController.navigate(Route.LevelSelection) {
+                    popUpTo(Route.TitleMenu) { inclusive = true }
+                }
             }
         }
 
         composable<Route.LevelSelection> {
-            LevelSelectionScreen {
-
+            LevelSelectionScreen(
+                modifier = Modifier
+                    .padding(innerPadding)
+            ) {
+                navController.navigate(Route.InGame) {
+                    popUpTo(Route.LevelSelection) { inclusive = true }
+                }
             }
         }
 
         composable<Route.InGame> {
-            InGameScreen()
+            val characterPortraitIds: List<Int> = listOf(
+                R.drawable.mini_girl,
+                R.drawable.mini_widow,
+                R.drawable.mini_woman,
+                R.drawable.mini_brotel,
+                R.drawable.mini_dancer,
+                R.drawable.mini_singer,
+                R.drawable.mini_washer,
+                R.drawable.mini_young_man,
+                R.drawable.mini_mercenary,
+                R.drawable.mini_teacher,
+                R.drawable.mini_peerage,
+                R.drawable.mini_servant,
+                R.drawable.mini_village_girl,
+                R.drawable.mini_sword_woman,
+                R.drawable.mini_embroidery,
+                R.drawable.mini_musician
+            ).shuffled()
+            InGameScreen(
+                modifier = Modifier
+                    .padding(innerPadding),
+                characterPortraitIds = characterPortraitIds
+            )
         }
     }
 }
 
+@Serializable
 sealed interface Route {
     @Serializable
     data object TitleMenu: Route
