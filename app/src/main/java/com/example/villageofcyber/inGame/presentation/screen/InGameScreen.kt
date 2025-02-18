5e41346906158000042350c1f46f5b7ca53307d2
execution720 <JustTomGo@gmail.com>
2025-02-18 15:35:20 +0900

    refactor(R.drawable.mini_brothel): rename typing error

--- app/src/main/java/com/example/villageofcyber/inGame/presentation/screen/InGameScreen.kt
+++ app/src/main/java/com/example/villageofcyber/inGame/presentation/screen/InGameScreen.kt
@@ -121,7 +121,7 @@ private fun InGameScreenPreview() {
         R.drawable.mini_girl,
         R.drawable.mini_widow,
         R.drawable.mini_woman,
-        R.drawable.mini_brotel,
+        R.drawable.mini_brothel,
         R.drawable.mini_dancer,
         R.drawable.mini_singer,
         R.drawable.mini_washer,
