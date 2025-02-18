4df0859bdd3da02aa55e044b6e672552a23f852a
execution720 <JustTomGo@gmail.com>
2025-02-18 13:53:23 +0900

    feat(Character): add Role enum class

--- app/src/main/java/com/example/villageofcyber/inGame/domain/Character.kt
+++ app/src/main/java/com/example/villageofcyber/inGame/domain/Character.kt
@@ -1,10 +1,21 @@
 package com.example.villageofcyber.inGame.domain
 
+enum class Role {
+    FIRSTBLOOD,
+    CITIZEN,
+    COWORKER,
+    PROPHET,
+    TRAITOR,
+    HUNTER,
+    WOLF,
+    BETRAYER
+}
+
 data class Character(
     val name: String,
     val miniFace: Int,
     val bigFace: Int,
     val alive: Boolean,
-    val role: Int,
-    val fakeRole: Int
+    val role: Role,
+    val fakeRole: Role
 )
